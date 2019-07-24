b96d79b464157c5cfb3c0fa334e9be9fe55bda64
cwksean10596 <x>
2019-07-24 12:01:51 -0700

    Created Ship

---
+++ New Game THing.py
@@ -0,0 +1,29 @@
+#pygame setup here
+import pygame
+from pygame.locals import *
+from Ship import Ship
+pygame.init()
+screen_info = pygame.display.Info()
+size = (width, height) = (int(screen_info.current_w),int(screen_info.current_h))
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+color = (50, 100, 100)
+
+LevelData = df.iloc[Level]
+Player = Ship (((LevelData['PlayerX'], LevelData['PlayerY'])))
+
+def main():
+    while True:
+        global color
+        clock.tick(60)
+        screen.fill(color)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit();
+                sys.exit()
+                main = False
+
+if __name__ == "__main__":
+    main()
---
+++ Ship.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Ship(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pygame.image.load('saucer2.png')
+        self.image = pygame.transform.smoothscale(self.image(40,40))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.speed = pygame.math.Vector2(0,0)
+
+    def update(self):
+        self.rect.move_ip(self.speed)
